a62528c4ba2c190e3f943922b1cf651ce07515c9
gayatrijadhav2102-hub <gayatrijadhav2102@gmail.com>
2026-01-17 13:50:23 +0530

    Delete crud directory

--- crud/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Student
-
-
-class StudentSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = "__all__"
